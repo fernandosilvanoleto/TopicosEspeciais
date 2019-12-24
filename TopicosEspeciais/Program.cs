3bc0a161e4f6cc68e0bf865b27b16b0a49c40e37
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2019-12-24 12:33:33 -0300

    Implementando controle de leitura e cópia de arquivo .txt.

---
+++ TopicosEspeciais/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace TopicosEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sintaxe @ evita colocar duas barras seguidas
+            string sourcePath = @"G:\Programação_Udemy\C#\TopicosEspeciais\Files\File_01.txt";
+            string targetPath = @"G:\Programação_Udemy\C#\TopicosEspeciais\Files\File_02.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
