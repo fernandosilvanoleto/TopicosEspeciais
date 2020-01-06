beecc3064495d17fb3c46f1d94f1ed46f4c48311
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-01-06 11:40:31 -0300

    Implementando Expressão LAMBDA.

---
+++ TopicosEspeciais/LAMBDA/Entities/ProductLAMBDA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace TopicosEspeciais.LAMBDA.Entities
+{
+    class ProductLAMBDA
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public ProductLAMBDA(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- TopicosEspeciais/Program.cs
+++ TopicosEspeciais/Program.cs
@@ -8,6 +8,7 @@ using TopicosEspeciais.Model.Entities_Her_Inter;
 using TopicosEspeciais.Model.Entities_Her_Multipla;
 using TopicosEspeciais.Model.Entities_IComparable;
 using TopicosEspeciais.Model.Enums;
+using TopicosEspeciais.LAMBDA.Entities;
 //using TopicosEspeciais.Extensions;
 
 namespace TopicosEspeciais
@@ -18,13 +19,36 @@ namespace TopicosEspeciais
         {
             try
             {
-                Console.WriteLine("Olá Bom Dia Mundo!!!");
+                List<ProductLAMBDA> list = new List<ProductLAMBDA>();
+                list.Add(new ProductLAMBDA("TV", 900.00));
+                list.Add(new ProductLAMBDA("Notebook", 1200.00));
+                list.Add(new ProductLAMBDA("Tablet", 450.00));
+
+                // EXPRESSÃO LAMBDA - uma função anônima, só passa os modificadores
+                // Comparison<ProductLAMBDA> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+                // USO EXPRESSÃO LAMBDA AQUI - NA REFERÊNCIA DO SORT (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())
+                list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())); 
+                // nesse caso, o SORT só funciona no ProductLAMBDA se tiver a CLASSE IComparable
+                // referência para uma função - chama-se DELEGATE
+
+
+
+                foreach (ProductLAMBDA item in list)
+                {
+                    Console.WriteLine(item);
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine("Error: ");
                 Console.WriteLine(e.Message);
             }
+        }
+
+        static int CompareProducts(ProductLAMBDA p1, ProductLAMBDA p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
