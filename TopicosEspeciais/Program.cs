e6a941f4caa7b221d4efe8562a7f6dbd9d63a405
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-01-06 12:32:00 -0300

    Implementando a Programação Funcional e o Cálculo LAMBDA.

--- TopicosEspeciais/Program.cs
+++ TopicosEspeciais/Program.cs
@@ -15,10 +15,15 @@ namespace TopicosEspeciais
 {
     class Program
     {
+        public static int globalValue = 3;
         static void Main(string[] args)
         {
             try
             {
+                /*
+                 * IMPLEMENTANDO A EXPRESSÃO LAMBDA - 06/01/2020
+                 
+                
                 List<ProductLAMBDA> list = new List<ProductLAMBDA>();
                 list.Add(new ProductLAMBDA("TV", 900.00));
                 list.Add(new ProductLAMBDA("Notebook", 1200.00));
@@ -28,16 +33,22 @@ namespace TopicosEspeciais
                 // Comparison<ProductLAMBDA> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
                 // USO EXPRESSÃO LAMBDA AQUI - NA REFERÊNCIA DO SORT (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())
-                list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())); 
+                list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
                 // nesse caso, o SORT só funciona no ProductLAMBDA se tiver a CLASSE IComparable
-                // referência para uma função - chama-se DELEGATE
-
+                // referência para uma função - chama-se DELEGATE        
 
 
                 foreach (ProductLAMBDA item in list)
                 {
                     Console.WriteLine(item);
                 }
+
+                 */
+
+                int[] vect = new int[] { 3, 4, 5 };
+                ChangeOddValues(vect);
+                Console.WriteLine(string.Join(" ", vect));
+
             }
             catch (Exception e)
             {
@@ -49,6 +60,17 @@ namespace TopicosEspeciais
         static int CompareProducts(ProductLAMBDA p1, ProductLAMBDA p2)
         {
             return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
+
+        public static void ChangeOddValues(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    numbers[i] += globalValue;
+                }
+            }
         }
     }
 }
