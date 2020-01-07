b0ffd8f81612e3486e97549f7c61bde84283aa00
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-01-07 10:59:51 -0300

    Implementando DELEGATE e o Multiple DELEGATE.

--- TopicosEspeciais/Program.cs
+++ TopicosEspeciais/Program.cs
@@ -13,6 +13,11 @@ using TopicosEspeciais.LAMBDA.Entities;
 
 namespace TopicosEspeciais
 {
+    // declaração de um delegate
+    delegate double BinaryNumericOperationSimple(double n1, double n2);
+
+    //Multiple Delegate só trabalha com VOID
+    delegate void BinaryNumericOperationMultiple(double n1, double n2);
     class Program
     {
         public static int globalValue = 3;
@@ -22,6 +27,7 @@ namespace TopicosEspeciais
             {
                 /*
                  * IMPLEMENTANDO A EXPRESSÃO LAMBDA - 06/01/2020
+                 * using TopicosEspeciais.LAMBDA.Entities;
                  
                 
                 List<ProductLAMBDA> list = new List<ProductLAMBDA>();
@@ -30,9 +36,11 @@ namespace TopicosEspeciais
                 list.Add(new ProductLAMBDA("Tablet", 450.00));
 
                 // EXPRESSÃO LAMBDA - uma função anônima, só passa os modificadores
+
                 // Comparison<ProductLAMBDA> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
                 // USO EXPRESSÃO LAMBDA AQUI - NA REFERÊNCIA DO SORT (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())
+
                 list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
                 // nesse caso, o SORT só funciona no ProductLAMBDA se tiver a CLASSE IComparable
                 // referência para uma função - chama-se DELEGATE        
@@ -43,11 +51,51 @@ namespace TopicosEspeciais
                     Console.WriteLine(item);
                 }
 
-                 */
+                 
+                 
+                // CÁLCULO LAMBDA
 
                 int[] vect = new int[] { 3, 4, 5 };
                 ChangeOddValues(vect);
                 Console.WriteLine(string.Join(" ", vect));
+
+                */
+                /*
+                 * Trabalhando com a DELEGATES - 07/01/2020
+                 * DELEGATE -> uma referência (com type safety para um ou mais métodos), ou seja, é um tipo referência
+                 * using TopicosEspeciais.LAMBDA.Entities;
+                 */
+
+                double a = 10;
+                double b = 12;
+
+                //objeto op é uma referência agora para a função CalculationService
+                BinaryNumericOperationSimple op = CalculationService.Sum;
+                //PODE USAR A FUNÇÃO INVOKE - CHAMA A FUNÇÃO DO "SUM"
+
+                double result = op(a, b);
+                //double result = op.Invoke(a, b);
+                Console.WriteLine("SIMPLE DELEGATE: " + result);
+
+
+                /* MULTICAST DELEGATES
+                 * 
+                 * Delegates que guardam a referência para mais de um método
+                 * Para adicionar uma referência, pode-se usar o operador +=
+                 * A chamada Invoke (ou sintaxe reduzida) executa todos os métodos na ordem em que foram adicionados
+                 * Seu uso faz sentido para métodos VOID
+                 */
+
+                double aa = 10;
+                double bb = 12;
+
+                //objeto op é uma referência agora para a função CalculationService
+                BinaryNumericOperationMultiple opMultiple = CalculationService.ShowSum;
+
+                opMultiple += CalculationService.ShowMax;
+
+                opMultiple.Invoke(aa, bb);
+                //opMultiple(aa, bb);
 
             }
             catch (Exception e)
